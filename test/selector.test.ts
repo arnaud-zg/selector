39403c04ef08419b2fd1212685d10c0b7587de50
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-11-10 21:08:59 +0100

    test(unit): update unit test snapshot

--- test/selector.test.ts
+++ test/selector.test.ts
@@ -24,12 +24,6 @@ describe('createSelector', () => {
     expect(firstElementSelector<string[]>([])).toBeUndefined();
   });
 
-  it('should select first element price', () => {
-    const firstElementSelector = createSelector(0);
-    expect(firstElementSelector(["It's me"])).toMatchSnapshot();
-    expect(firstElementSelector<string[]>(["It's me"])).toMatchSnapshot();
-  });
-
   it('should select first item price', () => {
     const data: IData = {
       items: [
